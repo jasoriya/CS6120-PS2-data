7576762733906fe82889e3aebc88b3c020184151
Shreyans Jasoriya <shreyansjasoriya@gmail.com>
2020-02-11 16:43:26 -0500

    Get file names

--- utils/lm_helper.py
+++ utils/lm_helper.py
@@ -17,12 +17,18 @@ from nltk.corpus import mac_morpho, brown, gutenberg
     
 def get_test_data(num_brown = 200, num_mac = 20):
     test_list = []
+    file_list = []
     for file in brown.fileids()[:num_brown]:
         test_list.append(list(brown.sents(file)))
+        file_list.append(file)
     for file in mac_morpho.fileids()[:num_mac]:
         test_list.append(list(mac_morpho.sents(file)))
-    random.shuffle(test_list)
-    return test_list
+        file_list.append(file)
+    zip_shuffler = list(zip(test_list, file_list))
+    random.shuffle(zip_shuffler)
+    test_list, file_list = zip(*zip_shuffler)
+    return test_list, file_list
+
 
 def get_train_data():
     train_list = []
